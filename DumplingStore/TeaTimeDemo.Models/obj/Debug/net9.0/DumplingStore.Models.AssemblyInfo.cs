2d429fa35f2da039b4dfbd2bee5e33bb256a390f
THS8901 <ths881130@gmail.com>
2025-04-13 17:48:32 +0800

    20250413-2_修正檔案總管顯示問題

--- DumplingStore/TeaTimeDemo.Models/obj/Debug/net9.0/DumplingStore.Models.AssemblyInfo.cs
+++ DumplingStore/TeaTimeDemo.Models/obj/Debug/net9.0/DumplingStore.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DumplingStore.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de6394d823e5b60b6384551023fc0e1d0138b643")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6529f1d6f9599326cc0c4488b29af4f0bf0d7d8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DumplingStore.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DumplingStore.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ DumplingStore/TeaTimeDemo/Areas/Admin/Controllers/StoreController.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using DumplingStore.DataAccess;
+using DumplingStore.DataAccess.Data;
+using DumplingStore.DataAccess.Repository;
+using DumplingStore.DataAccess.Repository.IRepository;
+using DumplingStore.Models;
+using DumplingStore.Models.ViewModels;
+using DumplingStore.Utility;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DumplingStore.Areas.Admin.Controllers//DumplingStore.DataAccess.DataAccess.Controllers
+{
+    [Area("Admin")]
+   // [Authorize(Roles = SD.Role_Admin)]
+    public class StoreController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public StoreController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Store> objStoreList = _unitOfWork.Store.GetAll().ToList();
+            return View(objStoreList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+           
+         
+            if(id == null || id == 0)
+            {
+                return View(new Store());
+            }
+            else
+            {
+                // 執行編輯
+                Store storeObj = _unitOfWork.Store.Get(u => u.Id == id);
+                return View();
+            }
+            
+        }
+        [HttpPost]
+        public IActionResult Upsert(Store storeObj)
+        {
+           
+
+            if (ModelState.IsValid)
+            {
+                if (storeObj.Id == 0)
+                {
+                    _unitOfWork.Store.Add(storeObj);
+
+                    
+                }
+                else
+                {
+                    _unitOfWork.Store.Update(storeObj);
+                }
+                _unitOfWork.Save();
+                TempData["success"] = "店鋪新增成功!";
+                return RedirectToAction("Index");
+                
+            }
+            else
+            {
+                return View(storeObj);
+            }
+         
+        }
+
+       /* public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? ProductFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "類別新增成功!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }*/
+
+        /*public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product categoryFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "類別刪除成功!";
+            return RedirectToAction("Index");
+        }*/
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Store> objStoreList = _unitOfWork.Store.GetAll().ToList();
+            return Json(new{data = objStoreList });
+        }
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var StoreToBeDeleted = _unitOfWork.Store.Get(u => u.Id == id);
+            if (StoreToBeDeleted == null)
+            {
+                return Json(new{success = false, message = "刪除失敗"});
+
+            }
+           
+            _unitOfWork.Store.Remove(StoreToBeDeleted);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "刪除成功" });
+        }
+    #endregion
+
+}
+}
