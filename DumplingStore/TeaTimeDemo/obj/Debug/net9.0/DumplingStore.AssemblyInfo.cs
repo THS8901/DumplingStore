f2bc690731a8d6da595a6ea56f7f44628078ccc0
THS8901 <ths881130@gmail.com>
2025-04-15 12:19:58 +0800

    20250415_開發團隊介紹內容與其他版面修正
    1.開發團隊介紹內容修正
    2.navbar 九宮聚香鍋貼水餃館商標加入
    3.最新消息頁面九宮聚香鍋貼水餃館商標加入

--- DumplingStore/TeaTimeDemo/obj/Debug/net9.0/DumplingStore.AssemblyInfo.cs
+++ DumplingStore/TeaTimeDemo/obj/Debug/net9.0/DumplingStore.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DumplingStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ad03ace7f0b2ba7fd340bae493ec490797f5a45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee587ff2d3cf3fb99e78d6a763afd0f3519459d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DumplingStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DumplingStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
